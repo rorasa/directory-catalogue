aaf160aa4ff7f5cbf8ea5801c3b27650184938f3
Wattanit Hotrakool <wattanit@wattanit.com>
2020-10-24 23:13:31 +0700

    Add basic default output file writer

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,6 @@
 use std::fs;
+use std::fs::File;
+use std::io::prelude::*;
 use std::path::PathBuf;
 
 struct Page {
@@ -25,7 +27,15 @@ fn build_page(pages: &mut Vec<Page>, path: &PathBuf, is_root: bool) -> () {
 
     for item in fs::read_dir(path).expect("FAILED: Cannot read content of input path."){
         if let Ok(item) = item {
-            children_pages.push(item.path());
+            let item_path = item.path();
+
+            if item_path.is_dir(){
+                children_pages.push(item_path);
+            }else{
+                if get_extension(&item_path) == "toml"{
+                    println!("Found TOML file")
+                }
+            }
         }
     }
 
@@ -44,15 +54,48 @@ fn build_page(pages: &mut Vec<Page>, path: &PathBuf, is_root: bool) -> () {
 
     // build child pages
     for sub_path in children_pages{
-        if sub_path.is_dir() {
-            build_page(pages, &sub_path, false);
-        } else{
-            if get_extension(&sub_path) == "toml"{
-                println!("Found TOML file")
-            }
-        }
-        
+        build_page(pages, &sub_path, false);
     }
+}
+
+fn create_default_output(pages: &Vec<Page>, root_path: &PathBuf) -> std::io::Result<()> {
+    let mut output_path = root_path.clone();
+    output_path.push("catalogue");
+    output_path.push("md");
+
+    // clear previous output
+    println!("Create default output (Markdown stack) at {}", output_path.display());
+    if output_path.exists(){
+        fs::remove_dir_all(&output_path)?;
+    }
+    fs::create_dir_all(&output_path)?;
+
+    // create home page
+    let fav_path = output_path.clone().join("favourite");
+    let tag_path = output_path.clone().join("tags");
+    let dir_path = output_path.clone().join("directory");
+
+    fs::create_dir(fav_path)?;
+    fs::create_dir(tag_path)?;
+    fs::create_dir(dir_path)?;
+
+    let mut output_file = File::create(output_path.clone().join("Readme.md"))?;
+    let mut output_page = format!("# Directory Catalogue\n\n");
+    output_page.push_str("[Favourite](favourite)\n\n");
+    output_page.push_str("[Tags](tags)\n\n");
+    output_page.push_str("[Directory](directory)\n\n");
+
+    output_file.write_all(output_page.as_bytes())?;
+
+    // print page tree
+    for page in pages{
+        println!("Page: {}", page);
+        for sub_page in &page.children{
+            println!("      --{}", sub_page.display());
+        }
+    }
+
+    Ok(())
 }
 
 pub fn run(root_path: &PathBuf){
@@ -61,15 +104,10 @@ pub fn run(root_path: &PathBuf){
     
     println!("Working on root: {}", root_path.display());
 
-    // add root page
-    build_page(&mut pages, root_path, true);
+    // build pages
+    build_page(&mut pages, &root_path, true);
 
-    // print page tree
-    for page in pages{
-        println!("Page: {}", page);
-        for sub_page in page.children{
-            println!("      --{}", sub_page.display());
-        }
-    }
+    // create default output
+    create_default_output(&pages, &root_path).expect("FAILED: Cannot successfully create output catalogue.");
 
 }
